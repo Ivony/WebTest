9236e510348a53471db3f074114dc911c9a5d67e
Ivony <Ivony@live.com>
2013-11-01 14:19:59 +0800

    初始化

---
+++ Ivony.Web.Test/Ivony.Web.Test/TestAssert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Web.Test
+{
+  public class TestAssert
+  {
+
+    internal TestAssert()
+    {
+
+    }
+
+    public void Error( string format, params object[] args )
+    {
+      throw new TestAssertFailureException( string.Format( CultureInfo.InvariantCulture, format, args ) );
+    }
+
+
+  }
+}
---
+++ Ivony.Web.Test/Ivony.Web.Test/TestInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Web.Test
+{
+  public class TestInfo
+  {
+
+    public string Name
+    {
+      get;
+      set;
+    }
+
+  }
+}
