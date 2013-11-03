a064e68b4b7391abcb44436e9d54948ffff9d81c
Ivony <Ivony@live.com>
2013-11-03 15:38:40 +0800

    整理断言

---
+++ Ivony.Web.Test/Asserts/EqualsAsserts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Web.Test
+{
+  public static class EuqalsAsserts
+  {
+
+
+    private const string _message = "断言失败，两个对象不相等";
+
+
+    public static void AreEqual<T>( this TestAssert assert, T obj1, T obj2, string message = null )
+    {
+
+      if ( object.Equals( obj1, obj2 ) )
+        return;
+
+      message = message ?? _message;
+
+      assert.Failure( message );
+
+    }
+
+  }
+}
---
+++ Ivony.Web.Test/Asserts/SetAsserts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Web.Test
+{
+  public static class SetAsserts
+  {
+
+
+    public static void HasAny<T>( this TestAssert assert, IEnumerable<T> set, string message = null )
+    {
+      if ( set.Any() )
+        return;
+
+      assert.Failure( message ?? "断言失败，集合没有任何元素" );
+    }
+
+    public static void HasAny<T>( this TestAssert assert, IEnumerable<T> set, Func<T, bool> predicate, string message = null )
+    {
+      if ( set.Any( predicate ) )
+        return;
+
+      assert.Failure( message ?? "断言失败，集合没有满足指定条件的元素" );
+    }
+
+    public static void Contains<T>( this TestAssert assert, IEnumerable<T> set, T element, string message )
+    {
+      if ( set.Contains( element ) )
+        return;
+
+      assert.Failure( message ?? "断言失败，集合不包含指定的元素" );
+    }
+
+
+    public static void IsSubsetOf<T>( this TestAssert assert, IEnumerable<T> set, IEnumerable<T> sub, string message )
+    {
+      if ( set.Intersect( sub ).Count() == sub.Count() )
+        return;
+
+      assert.Failure( message ?? "断言失败，验证的集合不是指定集合的子集" );
+    }
+
+  }
+}
