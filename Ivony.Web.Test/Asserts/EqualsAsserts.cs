51eb04060b78763d1eba613743c5edffd68e5d12
Ivony <Ivony@live.com>
2013-11-04 21:44:51 +0800

    新增断言

--- Ivony.Web.Test/Asserts/EqualsAsserts.cs
+++ Ivony.Web.Test/Asserts/EqualsAsserts.cs
@@ -9,20 +9,48 @@ namespace Ivony.Web.Test
   public static class EuqalsAsserts
   {
 
-
-    private const string _message = "断言失败，两个对象不相等";
-
-
     public static void AreEqual<T>( this TestAssert assert, T obj1, T obj2, string message = null )
     {
 
-      if ( object.Equals( obj1, obj2 ) )
+      if ( EqualityComparer<T>.Default.Equals( obj1, obj2 ) )
         return;
 
-      message = message ?? _message;
+      assert.Failure( message ?? "断言失败，期望两个对象相等，但两个对象不相等" );
 
-      assert.Failure( message );
+    }
 
+
+    public static void AreNotEqual<T>( this TestAssert assert, T obj1, T obj2, string message = null )
+    {
+      if ( !EqualityComparer<T>.Default.Equals( obj1, obj2 ) )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望两个对象不相等，但两个对象相等" );
+    }
+
+    public static void NotNull<T>( this TestAssert assert, T obj, string message = null ) where T : class
+    {
+      if ( obj != null )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望对象不为 null，但对象是 null" );
+    }
+
+    public static void NotNull<T>( this TestAssert assert, T? obj, string message = null ) where T : struct
+    {
+      if ( obj.HasValue )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望对象有值，但对象是 null" );
+    }
+
+    public static void NotDbNull( this TestAssert assert, object obj, string message = null )
+    {
+      if ( obj == null )
+        assert.Failure( message ?? "断言失败，期望对象不为 null 或 DbNull，但对象是 null" );
+
+      if ( Convert.IsDBNull( obj ) )
+        assert.Failure( message ?? "断言失败，期望对象不为 null 或 DbNull，但对象是 DbNull" );
     }
 
   }
--- Ivony.Web.Test/Asserts/SetAsserts.cs
+++ Ivony.Web.Test/Asserts/SetAsserts.cs
@@ -15,7 +15,7 @@ namespace Ivony.Web.Test
       if ( set.Any() )
         return;
 
-      assert.Failure( message ?? "断言失败，集合没有任何元素" );
+      assert.Failure( message ?? "断言失败，期望集合至少有一个元素，但集合是空的" );
     }
 
     public static void HasAny<T>( this TestAssert assert, IEnumerable<T> set, Func<T, bool> predicate, string message = null )
@@ -23,7 +23,7 @@ namespace Ivony.Web.Test
       if ( set.Any( predicate ) )
         return;
 
-      assert.Failure( message ?? "断言失败，集合没有满足指定条件的元素" );
+      assert.Failure( message ?? "断言失败，期望集合至少有一个符合条件的元素，但集合没有满足指定条件的元素" );
     }
 
     public static void Contains<T>( this TestAssert assert, IEnumerable<T> set, T element, string message = null )
@@ -31,7 +31,7 @@ namespace Ivony.Web.Test
       if ( set.Contains( element ) )
         return;
 
-      assert.Failure( message ?? "断言失败，集合不包含指定的元素" );
+      assert.Failure( message ?? "断言失败，期望集合包含指定的元素，但集合不包含" );
     }
 
 
@@ -40,7 +40,7 @@ namespace Ivony.Web.Test
       if ( set.Intersect( sub ).Count() == sub.Count() )
         return;
 
-      assert.Failure( message ?? "断言失败，验证的集合不是指定集合的子集" );
+      assert.Failure( message ?? "断言失败，期望集合是指定集合的子集，但验证的集合含有指定集合之外的元素" );
     }
 
   }
