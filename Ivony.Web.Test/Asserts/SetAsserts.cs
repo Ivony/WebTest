a99bb088bf9883716e460901c6b031a245db3a95
Ivony <Ivony@live.com>
2013-11-05 14:29:58 +0800

    增加 Assert

--- Ivony.Web.Test/Asserts/SetAsserts.cs
+++ Ivony.Web.Test/Asserts/SetAsserts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,15 @@ namespace Ivony.Web.Test
   {
 
 
+    public static void HasAny( this TestAssert assert, IEnumerable set, string message = null )
+    {
+      foreach ( var item in set )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望集合至少有一个元素，但集合是空的" );
+    }
+
+
     public static void HasAny<T>( this TestAssert assert, IEnumerable<T> set, string message = null )
     {
       if ( set.Any() )
@@ -18,12 +28,21 @@ namespace Ivony.Web.Test
       assert.Failure( message ?? "断言失败，期望集合至少有一个元素，但集合是空的" );
     }
 
+    public static void HasAny( this TestAssert assert, IEnumerable set, Func<object, bool> predicate, string message = null )
+    {
+      if ( set.Cast<object>().Any() )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望集合至少有一个符合条件的元素，但集合没有任何满足指定条件的元素" );
+    }
+
+
     public static void HasAny<T>( this TestAssert assert, IEnumerable<T> set, Func<T, bool> predicate, string message = null )
     {
       if ( set.Any( predicate ) )
         return;
 
-      assert.Failure( message ?? "断言失败，期望集合至少有一个符合条件的元素，但集合没有满足指定条件的元素" );
+      assert.Failure( message ?? "断言失败，期望集合至少有一个符合条件的元素，但集合没有任何满足指定条件的元素" );
     }
 
     public static void Contains<T>( this TestAssert assert, IEnumerable<T> set, T element, string message = null )
@@ -35,13 +54,31 @@ namespace Ivony.Web.Test
     }
 
 
-    public static void IsSubsetOf<T>( this TestAssert assert, IEnumerable<T> set, IEnumerable<T> sub, string message = null )
+    public static void IsSubsetOf<T>( this TestAssert assert, ISet<T> set, ISet<T> sub, string message = null )
     {
-      if ( set.Intersect( sub ).Count() == sub.Count() )
+      if ( sub.IsSubsetOf( set ) )
         return;
 
       assert.Failure( message ?? "断言失败，期望集合是指定集合的子集，但验证的集合含有指定集合之外的元素" );
     }
+
+
+    public static void HasAll<T>( this TestAssert assert, IEnumerable<T> set, Func<T, bool> predicate, string message = null )
+    {
+      if ( set.All( predicate ) )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望所有元素都满足指定条件，但至少有一个元素不满足" );
+    }
+
+    public static void AllAreNotNull<T>( this TestAssert assert, IEnumerable<T> set, string message = null )
+    {
+      if ( set.All( i => i != null ) )
+        return;
+
+      assert.Failure( message ?? "断言失败，期望所有的元素都不为空，但至少一个元素为空" );
+    }
+
 
   }
 }
